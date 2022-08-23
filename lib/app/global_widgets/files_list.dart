6e897a269f2719b527d30f424cbfd894c2cf84e0
ASUS <ma.kh.inbox2@gmail.com>
2022-08-23 15:09:51 +0430

    fixed download path

--- lib/app/global_widgets/files_list.dart
+++ lib/app/global_widgets/files_list.dart
@@ -25,98 +25,99 @@ class FilesListView extends StatefulWidget {
 }
 
 class _FilesListViewState extends State<FilesListView> {
-  // final ReceivePort _port = ReceivePort();
-  //
-  // @override
-  // void initState() {
-  //   super.initState();
-  //
-  //   IsolateNameServer.registerPortWithName(_port.sendPort, 'downloader_send_port');
-  //   _port.listen((dynamic data) {
-  //     String id = data[0];
-  //     DownloadTaskStatus status = data[1];
-  //     int progress = data[2];
-  //     setState(() {});
-  //   });
-  //
-  //   FlutterDownloader.registerCallback(downloadCallback);
-  // }
-  //
-  // @override
-  // void dispose() {
-  //   IsolateNameServer.removePortNameMapping('downloader_send_port');
-  //   super.dispose();
-  // }
-  //
-  // static void downloadCallback(String id, DownloadTaskStatus status, int progress) {
-  //   final SendPort send = IsolateNameServer.lookupPortByName('downloader_send_port')!;
-  //   send.send([id, status, progress]);
-  // }
-  //
-  // void _download(String url) async {
-  //   final externalDir = await getExternalStorageDirectory();
-  //
-  //   final id = await FlutterDownloader.enqueue(
-  //     url: url,
-  //     headers: {
-  //       'X-token': (await StorageProvider.getAccessToken())!,
-  //     },
-  //     savedDir: externalDir!.path,
-  //     showNotification: true,
-  //     openFileFromNotification: false,
-  //   );
-  // }
-
-  late String _localPath;
-  late bool _permissionReady;
-  late TargetPlatform? platform;
+  final ReceivePort _port = ReceivePort();
 
   @override
   void initState() {
     super.initState();
-    if (Platform.isAndroid) {
-      platform = TargetPlatform.android;
-    } else {
-      platform = TargetPlatform.iOS;
-    }
+
+    IsolateNameServer.registerPortWithName(_port.sendPort, 'downloader_send_port');
+    _port.listen((dynamic data) {
+      String id = data[0];
+      DownloadTaskStatus status = data[1];
+      int progress = data[2];
+      setState(() {});
+    });
+
+    FlutterDownloader.registerCallback(downloadCallback);
   }
 
-  Future<bool> _checkPermission() async {
-    if (platform == TargetPlatform.android) {
-      final status = await Permission.storage.status;
-      if (status != PermissionStatus.granted) {
-        final result = await Permission.storage.request();
-        if (result == PermissionStatus.granted) {
-          return true;
-        }
-      } else {
-        return true;
-      }
-    } else {
-      return true;
-    }
-    return false;
+  @override
+  void dispose() {
+    IsolateNameServer.removePortNameMapping('downloader_send_port');
+    super.dispose();
   }
 
-  Future<void> _prepareSaveDir() async {
-    _localPath = (await _findLocalPath())!;
-
-    print(_localPath);
-    final savedDir = Directory(_localPath);
-    bool hasExisted = await savedDir.exists();
-    if (!hasExisted) {
-      savedDir.create();
-    }
+  static void downloadCallback(String id, DownloadTaskStatus status, int progress) {
+    final SendPort send = IsolateNameServer.lookupPortByName('downloader_send_port')!;
+    send.send([id, status, progress]);
   }
 
-  Future<String?> _findLocalPath() async {
-    if (platform == TargetPlatform.android) {
-      return "/sdcard/download/";
-    } else {
-      var directory = await getApplicationDocumentsDirectory();
-      return directory.path + Platform.pathSeparator + 'Download';
-    }
+  void _download(String url) async {
+    final externalDir = await getExternalStorageDirectory();
+
+    final id = await FlutterDownloader.enqueue(
+      url: url,
+      headers: {
+        'X-token': (await StorageProvider.getAccessToken())!,
+      },
+      savedDir: '/storage/emulated/0/Download/',
+      showNotification: true,
+      openFileFromNotification: false,
+    );
+    FlutterDownloader.open(taskId: id!);
   }
+
+  // late String _localPath;
+  // late bool _permissionReady;
+  // late TargetPlatform? platform;
+  //
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   if (Platform.isAndroid) {
+  //     platform = TargetPlatform.android;
+  //   } else {
+  //     platform = TargetPlatform.iOS;
+  //   }
+  // }
+  //
+  // Future<bool> _checkPermission() async {
+  //   if (platform == TargetPlatform.android) {
+  //     final status = await Permission.storage.status;
+  //     if (status != PermissionStatus.granted) {
+  //       final result = await Permission.storage.request();
+  //       if (result == PermissionStatus.granted) {
+  //         return true;
+  //       }
+  //     } else {
+  //       return true;
+  //     }
+  //   } else {
+  //     return true;
+  //   }
+  //   return false;
+  // }
+  //
+  // Future<void> _prepareSaveDir() async {
+  //   _localPath = (await _findLocalPath())!;
+  //
+  //   print(_localPath);
+  //   final savedDir = Directory(_localPath);
+  //   bool hasExisted = await savedDir.exists();
+  //   if (!hasExisted) {
+  //     savedDir.create();
+  //   }
+  // }
+  //
+  // Future<String?> _findLocalPath() async {
+  //   if (platform == TargetPlatform.android) {
+  //     return "/sdcard/download/";
+  //   } else {
+  //     var directory = await getApplicationDocumentsDirectory();
+  //     return directory.path + Platform.pathSeparator + 'Download';
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -128,29 +129,28 @@ class _FilesListViewState extends State<FilesListView> {
             onTap: () async {
               print("Downloading file.");
               print(widget.files[index].url);
-              // _download(Configs.getBaseUrl + "download/" + widget.files[index].url);
+              _download(Configs.getBaseUrl + "download/" + widget.files[index].url);
 
-              _permissionReady = await _checkPermission();
-              if (_permissionReady) {
-                await _prepareSaveDir();
-                print("Downloading");
-                try {
-                  Dio dio = new Dio();
-                  dio.options.headers["X-token"] = (await StorageProvider.getAccessToken())!;
-                  EasyLoading.show(status: Strings.LOADING, maskType: EasyLoadingMaskType.black, dismissOnTap: false);
-                  await dio.download(
-                    Configs.getBaseUrl + "download/" + widget.files[index].url,
-                    // _localPath + "/" + widget.files[index].url,
-                    '/storage/emulated/0/Download/' + widget.files[index].url,
-                  );
-                  print("Download Completed.");
-                  print(_localPath + "/" + widget.files[index].url);
-                } catch (e) {
-                  print("Download Failed.\n\n" + e.toString());
-                } finally {
-                  EasyLoading.dismiss();
-                }
-              }
+              // _permissionReady = await _checkPermission();
+              // if (_permissionReady) {
+              //   await _prepareSaveDir();
+              //   print("Downloading");
+              //   try {
+              //     Dio dio = new Dio();
+              //     dio.options.headers["X-token"] = (await StorageProvider.getAccessToken())!;
+              //     EasyLoading.show(status: Strings.LOADING, maskType: EasyLoadingMaskType.black, dismissOnTap: false);
+              //     await dio.download(
+              //       Configs.getBaseUrl + "download/" + widget.files[index].url,
+              //       // _localPath + "/" + widget.files[index].url,
+              //       '/storage/emulated/0/Download/' + widget.files[index].url,
+              //     );
+              //     print("Download Completed.");
+              //   } catch (e) {
+              //     print("Download Failed.\n\n" + e.toString());
+              //   } finally {
+              //     EasyLoading.dismiss();
+              //   }
+              // }
             },
             behavior: HitTestBehavior.translucent,
             child: FileListTile(
